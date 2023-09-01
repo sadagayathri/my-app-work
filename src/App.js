af9191003e1a5a4f540a4adf92c07953697b0168
sadagayathri <gayathrisada022@gmail.com>
2023-09-01 20:21:05 +0530

    hguij

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+ import 'bootstrap/dist/css/bootstrap.min.css';
+import Project from './Project';
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>welcome</h1>
+    
+    <div className="mybox">
+    
+      <Project></Project>
+  
     </div>
+
   );
 }
 
