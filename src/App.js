668e3238a08240cb81e32925f87fd34bcf42c23e
sadagayathri <gayathrisada022@gmail.com>
2023-09-01 21:38:22 +0530

    jgk

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Students from './Students';
 
 function App() {
   return (
-    <div className="App">
-      <h1>welcome</h1>
+    <div className="mybox">
+      <Students></Students>
+      
     </div>
   );
 }
---
+++ src/Students.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Students() {
+    var [students,setStudents]=React.useState(['Pallavi','Jaya','Dharma','Souwmya','Anu','Eshwar'])
+    return (
+        <center>
+            <div className='mybox' style={{width:"200px",height:"250px",fontFamily:"times new roman",boxShadow:"1px 10px 20px 2px"}}>
+            <h1><b>Students</b></h1>
+            
+            {
+                students.map((student)=>{
+                    return <li>{student}</li>
+                })
+            }
+        </div>
+        </center>
+    );
+}
+
+export default Students;
